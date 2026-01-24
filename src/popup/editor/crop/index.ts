eab56508f7478099c50ef7602c01dc387a399775
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2026-01-24 15:05:17 +0530

    feat: remove CropOverlay, CropTransform, and related types to streamline crop functionality

--- src/popup/editor/crop/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Crop tool module exports
- */
-
-export { CropTransform } from './CropTransform';
-export { CropOverlay } from './CropOverlay';
-export type { CropData, CropConfig, HandlePosition, DragType, Point, BoundaryRect } from './types';
-export { DEFAULT_CROP_CONFIG } from './types';
